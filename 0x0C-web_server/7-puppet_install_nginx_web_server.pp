42f010ce03372585591d486792de03212fa33035
julianfrancor <julianfrancor@gmail.com>
2020-08-05 22:25:50 -0500

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,28 @@
-# install the package
+#install nginx
+
 package { 'nginx':
   ensure   => 'latest',
   name     => 'nginx',
   provider => 'apt'
 }
 
+# create index
+
 file { 'index':
-  path    => '/var/www/html/index.nginx-debian.html',
-  mode    => '0644',
+  path    => '/var/www/html/index.html',
+  mode    => '0664',
   content => 'Holberton School'
 }
+
+# config redirection
 
 file_line { 'redirect_me':
   path  => '/etc/nginx/sites-available/default',
   line  => "\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;/n/t%7D",
   after => '^server {',
 }
+
+# start server.
 
 service { 'nginx':
   ensure => running,
