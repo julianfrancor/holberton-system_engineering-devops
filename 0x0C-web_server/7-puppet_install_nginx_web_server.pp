4c650bac6b134da89257cbaa524969788bd43996
julianfrancor <julianfrancor@gmail.com>
2020-08-05 22:08:16 -0500

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,14 +7,14 @@ package { 'nginx':
 }
 
 file { 'index.html':
-  path    => '/var/www/html/index.html',
+  path    => '/var/www/html/index.nginx-debian.html',
   mode    => '0644',
   content => 'Holberton School'
 }
 
 file_line { '301 Moved Permanently':
   path  => '/etc/nginx/sites-available/default',
-  line  => '/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line  => "\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;/n/t%7D",
   match => '^server {'
 }
 
