12fe3a87d96216d7ee6bf006d5bedb2cd358275b
julianfrancor <julianfrancor@gmail.com>
2020-09-17 15:38:32 -0500

    task 0: function that queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given
+subreddit. If an invalid subreddit is given, the function should
+return 0.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that queries the Reddit API and
+    returns the number of subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "iOSJulian:myappJulian:v1 (by /u/julianfrancor)"}
+    req = requests.get(URL, headers=headers)
+    about_dictionary = req.json()
+    total_subscribers = about_dictionary.get('data').get('subscribers')
+    if total_subscribers is None:
+        return 0
+    return total_subscribers
