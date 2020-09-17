2502ece47ff4f7d7708b0afed9b6e1ac5e792358
julianfrancor <julianfrancor@gmail.com>
2020-09-17 17:27:28 -0500

    task 1: function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and prints the titles of
+the first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Function that queries the Reddit API and prints the titles of
+    the first 10 hot posts"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "iOSJulian:myappJulian:v1 (by /u/julianfrancor)"}
+    req = requests.get(URL, headers=headers)
+    about_dictionary = req.json()
+    data = about_dictionary.get('data')
+    if data is None:
+        return None
+    childrens = data.get('children')
+    if childrens is None:
+        return None
+    for children in childrens:
+        myTitle = children.get('data').get('title')
+        print(myTitle)
