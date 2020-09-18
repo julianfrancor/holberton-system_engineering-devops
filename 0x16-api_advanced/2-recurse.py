8e1cdb2baf0a22ad01ccd5d847b3a4d8371b2955
julianfrancor <julianfrancor@gmail.com>
2020-09-17 19:16:17 -0500

    task 2: recursive function that queries an API

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -19,12 +19,10 @@ def recurse(subreddit, hot_list=[], after=None):
     about_dictionary = req.json()
     data = about_dictionary.get('data')
     if data is None:
-        print("None")
-        return
+        return None
     childrens = data.get('children')
     if childrens is None:
-        print("None")
-        return
+        return None
     for children in childrens:
         hot_list.append(children['data']['title'])
     af = data.get('after')
