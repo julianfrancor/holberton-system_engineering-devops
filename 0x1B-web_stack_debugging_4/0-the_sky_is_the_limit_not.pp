e1f5870351cf63a6cb4ebb5798b37d6a1ee302cb
julianfrancor <julianfrancor@gmail.com>
2020-10-08 12:14:34 -0500

    task 0: puppet manifest to fix ulimit in nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Using Puppet, to fix ulimit (Max Open FIles Limit) for Nginx on Ubuntu
 exec {'fix-ulimit':
-  path    => '/bin/',
+  path    => '/usr/bin/env',
   command => 'sed -i s/15/1000/ /etc/default/nginx',
 }
-exec {'restart nginx service':
+-> exec {'restart nginx service':
   path    => '/usr/bin/env',
   command => 'service nginx restart',
 }
